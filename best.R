325d58780262aeb3b1fd7e94e78b85b0bfdeafe8
Jose Maria Verde <jmverde@gmail.com>
2014-09-17 22:26:38 +0200

    Correct 1,3,  redo 2

---
+++ best.R
@@ -0,0 +1,40 @@
+best <- function(state, outcome) {
+## Read outcome data
+
+	data<-read.csv("outcome-of-care-measures.csv",colClasses = "character")
+## Check that state and outcome are valid
+
+## Make a list of states
+
+	states <- unique(data$State)
+	
+
+	if ((state%in%states)==FALSE) stop("invalid state")
+
+## List of outcomes 
+
+	outcomes <- c("heart attack","heart failure","pneumonia")
+
+	if((outcome%in%outcomes)==FALSE) stop("invalid outcome")
+
+## heart attack es el 11
+##heart failure es el 17
+## pneumonia es el 23
+
+if (outcome==outcomes[1]) outcomeindex<-11
+if (outcome==outcomes[2]) outcomeindex<-17
+if (outcome==outcomes[3]) outcomeindex<-23
+
+## Return hospital name in that state with lowest 30-day death
+## rate
+
+	splitOutcome<-split(data,data$State)
+	dataState<-splitOutcome[[state]]
+
+## preparo un dataset mas peque?o solo con las columnas que interesan
+## el mombre de hospital 2 y el outcomeindex
+
+    dataAn<-dataState[c(2,outcomeindex)]
+    dataAn<-dataAn[order(dataAn[,2],dataAn[,1]),]
+    dataAn[1,1]
+}
